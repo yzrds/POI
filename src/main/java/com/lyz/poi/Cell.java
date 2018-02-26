b13b4fad0231d3e3d79b52ffb60e13f92c3d5e47
lyzgit <youzel@126.com>
2018-02-26 21:01:33 +0800

    create sheet

---
+++ src/main/java/com/lyz/poi/Cell.java
@@ -0,0 +1,5 @@
+package com.lyz.poi;
+
+public class Cell {
+
+}
--- src/main/java/com/lyz/poi/Sheet.java
+++ src/main/java/com/lyz/poi/Sheet.java
@@ -1,7 +1,23 @@
 package com.lyz.poi;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Sheet {
     public static void main(String[] args) {
-        
+        // create a new workbook
+        Workbook workbook = new HSSFWorkbook();
+        workbook.createSheet("first sheet");
+        workbook.createSheet("second sheet");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("F:\\workbook1.xls");
+            workbook.write(fileOutputStream);
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
