a680df11f96774ab04767e7e509d2cdd6874a6bd
lyzgit <youzel@126.com>
2018-02-26 21:03:24 +0800

    create cell

--- src/main/java/com/lyz/poi/Cell.java
+++ src/main/java/com/lyz/poi/Cell.java
@@ -1,5 +1,13 @@
 package com.lyz.poi;
 
-public class Cell {
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 
+public class Cell {
+    public static void main(String[] args) {
+        // create a new workbook
+        Workbook workbook = new HSSFWorkbook();
+        workbook.createSheet("first sheet");
+        workbook.createSheet("second sheet");
+    }
 }
